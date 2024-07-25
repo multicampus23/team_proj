abb869d9d7e7c55ff2361fcee1db40f3816517a6
p.jangwoo@gmail.com <p.jangwoo@gmail.com>
2024-07-25 13:44:04 +0900

    초기셋팅 완료

---
+++ src/main/java/com/muticampus/team_proj/TeamProjApplication.java
@@ -0,0 +1,13 @@
+package com.muticampus.team_proj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamProjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/muticampus/team_proj/TeamProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muticampus.team_proj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
